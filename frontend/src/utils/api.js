bf6256610b7ed2ad69810b901a075abf7119675b
Tahyr Orazdurdyyev <orazdurdyyevtahyr@gmail.com>
2025-11-07 03:43:14 +0500

    Add debug logging to utils/api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,6 +2,7 @@ export const API_BASE_URL = "https://zootel.shop/api/v1";
 
 export const api = {
   get: async (url) => {
+    console.log('🚨 utils/api.js GET called with:', { url, fullUrl: `${API_BASE_URL}${url}` });
     const response = await fetch(`${API_BASE_URL}${url}`);
     return response.json();
   },
